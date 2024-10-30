375133eecee73a22af203506ba6ad447534249d7
Mutyala Kalyani <mutyalakalyani211@gmail.com>
2024-10-30 13:22:39 +0530

    first commit

---
+++ breast_cancer_classification_website/frontend_ml/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css"
+import HomePage from './components/HomePage'
+const App = () => {
+  return (
+    <div className='app'>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
